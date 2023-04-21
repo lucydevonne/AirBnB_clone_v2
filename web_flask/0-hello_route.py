8bbb127ae95435efd2ea87489d94615700ad2fa7
lucydevonne <wambuilucie7@gmail.com>
2023-04-21 17:37:15 +0000

    web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" a script that starts a Flask web application
+web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
+"""
 
 from flask import Flask
 
