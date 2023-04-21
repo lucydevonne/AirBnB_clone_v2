00fa6658abe3fc331c8f71c87333bda02e1d3938
lucydevonne <wambuilucie7@gmail.com>
2023-04-21 19:29:41 +0000

    web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ You must use the option strict_slashes=False in your route definition
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
