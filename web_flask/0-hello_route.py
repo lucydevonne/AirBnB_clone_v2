fdc47e6f1a050bbfea9086265b1129804a979816
lucydevonne <wambuilucie7@gmail.com>
2023-04-24 04:00:53 +0000

    web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,18 +2,20 @@
 """ a script that starts a Flask web application
 web application must be listening on 0.0.0.0, port 5000
 Routes:
-/: display “Hello HBNB!”
+/: display Hello HBNB!
 You must use the option strict_slashes=False in your route definition
 """
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-	"""display “Hello HBNB!”"""
-	return "Hello HBNB!"
+	"""display Hello HBNB!"""
+	return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000,)
