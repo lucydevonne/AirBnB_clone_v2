296fea1e51153419e2d19c6a7463626c5be69cab
lucydevonne <wambuilucie7@gmail.com>
2023-04-26 08:13:39 +0000

    web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,7 +6,8 @@ The web application must be listening on 0.0.0.0, port 5000.
 Routes:
     /: Display "Hello HBNB!"
     /hbnb: Display "HBNB"
-    /c/<text>: Display "C " followed by the value of the text variable (replace underscore _ symbols with a space)
+    /c/<text>: Display "C " followed by the value of the text variable 
+             (replace underscore _ symbols with a space)
 
 You must use the option strict_slashes=False in your route definition.
 """
