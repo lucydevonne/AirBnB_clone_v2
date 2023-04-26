a8497d4ddb6909b71c7ea02296e5bacb5ec7ddd7
lucydevonne <wambuilucie7@gmail.com>
2023-04-26 08:03:53 +0000

    web framework

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-You must use the option strict_slashes=False in your route definition
+""" Write a script that starts a Flask web application
 """
 
-import Flask from flask
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
+
 
 @app.route('/', strict_slashes=False)
-def hello_world():
-	return 'Hello HBNB!'
+def hello():
+    """Return HBNB!"""
+    return ("Hello HBNB!")
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-	return 'HBNB'
+    """Return HBNB"""
+    return ("HBNB")
 
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, )
