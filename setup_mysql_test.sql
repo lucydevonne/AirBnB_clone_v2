c0f927601709174227f3a3211f4588113b5cbd97
lucydevonne <wambuilucie7@gmail.com>
2023-04-29 13:26:39 -0700

    sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,15 +1,12 @@
-# Creates the database hbnb_test_db
+-- this script prepares a MySQL server for the project
+-- create project testing database with the name : hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
-# Creates the user hbnb_test and sets the password to hbnb_test_pwd
+-- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
-# Grants all privileges to hbnb_test on the database hbnb_test_db
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
-# Grants SELECT privilege to hbnb_test on the database performance_schema
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-
-# Flushes the privileges to ensure that the changes take effect
 FLUSH PRIVILEGES;
-
+-- granting all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
