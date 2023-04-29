55d4a50fde3f59e114ded65bfcf46e898525bee2
lucydevonne <wambuilucie7@gmail.com>
2023-04-29 15:09:26 -0700

    sql

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 
-class Amenity(BaseModel): 
+
+class Amenity(BaseModel):
     """Amenity class for storing information about amenities."""
     name = ""
